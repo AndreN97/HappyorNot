aad6682bcda8da0fcf1a4b328cc5fc5ce0a301af
AndreN97 <97noan@skola.engelholm.se>
2017-09-11 14:48:19 +0200

    Början på sidans gränssnitt

--- result.php
+++ result.php
@@ -5,21 +5,5 @@ $ansThree = $_POST['qThree'];
 $ansFour = $_POST['qFour'];
 $ansFive = $_POST['qFive'];
 
-switch($_POST){
-    case "qOne":
-        header('Location: questions2.html');
-        break;
-    case "qTwo":
-        header('Location: questions3.html');
-        break;
-    case "qThree":
-        header('Location: Questions4.html');
-        break;
-    case "qFour":
-        header('Location: questions5.html');
-        break;
-    default:
-        header('Location: questions.php');
-        break;
-    }
+echo "Ditt svar har tagits emot, tack!"
 ?>
