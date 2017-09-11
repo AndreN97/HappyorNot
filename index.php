d3e50924e3ffcac433327b6c074d7796c178952e
AndreN97 <97noan@skola.engelholm.se>
2017-09-11 08:46:53 +0200

    Början på sidans gränssnitt

--- index.php
+++ index.php
@@ -7,41 +7,66 @@
     <body>
 
         <form action="result.php" method="post">
-            <label for="qOne">Fråga 1</label><br>
-            <input type="radio" name="qOne" value="one">1<br>
-            <input type="radio" name="qOne" value="two">2<br>
-            <input type="radio" name="qOne" value="three">3<br>
-            <input type="radio" name="qOne" value="four">4<br>
-            <input type="radio" name="qOne" value="five">5<br><br>
+            <label for="qOne">Fråga 1</label><br><br>
+            <input type="radio" name="qOne" id="q1a1" value="one">
+            <label for="q1a1">1</label>
+            <input type="radio" name="qOne" id="q1a2" value="two">
+            <label for="q1a2">2</label>
+            <input type="radio" name="qOne" id="q1a3" value="three">
+            <label for="q1a3">3</label>
+            <input type="radio" name="qOne" id="q1a4" value="four">
+            <label for="q1a4">4</label>
+            <input type="radio" name="qOne" id="q1a5" value="five">
+            <label for="q1a5">5</label><br><br><br>
 
-            <label for="qTwo">Fråga 2</label><br>
-            <input type="radio" name="qTwo" value="one">1<br>
-            <input type="radio" name="qTwo" value="two">2<br>
-            <input type="radio" name="qTwo" value="three">3<br>
-            <input type="radio" name="qTwo" value="four">4<br>
-            <input type="radio" name="qTwo" value="five">5<br><br>
+            <label for="qTwo">Fråga 2</label><br><br>
+            <input type="radio" name="qTwo" id="q2a1" value="one">
+            <label for="q2a1">1</label>
+            <input type="radio" name="qTwo" id="q2a2" value="two">
+            <label for="q2a2">2</label>
+            <input type="radio" name="qTwo" id="q2a3" value="three">
+            <label for="q2a3">3</label>
+            <input type="radio" name="qTwo" id="q2a4" value="four">
+            <label for="q2a4">4</label>
+            <input type="radio" name="qTwo" id="q2a5" value="five">
+            <label for="q2a5">5</label><br><br><br>
 
-            <label for="qThree">Fråga 3</label><br>
-            <input type="radio" name="qThree" value="one">1<br>
-            <input type="radio" name="qThree" value="two">2<br>
-            <input type="radio" name="qThree" value="three">3<br>
-            <input type="radio" name="qThree" value="four">4<br>
-            <input type="radio" name="qThree" value="five">5<br><br>
+            <label for="qThree">Fråga 3</label><br><br>
+            <input type="radio" name="qThree" id="q3a1" value="one">
+            <label for="q3a1">1</label>
+            <input type="radio" name="qThree" id="q3a2" value="two">
+            <label for="q3a2">2</label>
+            <input type="radio" name="qThree" id="q3a3" value="three">
+            <label for="q3a3">3</label>
+            <input type="radio" name="qThree" id="q3a4" value="four">
+            <label for="q3a4">4</label>
+            <input type="radio" name="qThree" id="q3a5" value="five">
+            <label for="q3a5">5</label><br><br><br>
 
-            <label for="qFour">Fråga 4</label><br>
-            <input type="radio" name="qFour" value="one">1<br>
-            <input type="radio" name="qFour" value="two">2<br>
-            <input type="radio" name="qFour" value="three">3<br>
-            <input type="radio" name="qFour" value="four">4<br>
-            <input type="radio" name="qFour" value="five">5<br><br>
+            <label for="qFour">Fråga 4</label><br><br>
+            <input type="radio" name="qFour" id="q4a1" value="one">
+            <label for="q4a1">1</label>
+            <input type="radio" name="qFour" id="q4a2" value="two">
+            <label for="q4a2">2</label>
+            <input type="radio" name="qFour" id="q4a3" value="three">
+            <label for="q4a3">3</label>
+            <input type="radio" name="qFour" id="q4a4" value="four">
+            <label for="q4a4">4</label>
+            <input type="radio" name="qFour" id="q4a5" value="five">
+            <label for="q4a5">5</label><br><br><br>
 
-            <label for="qFive">Fråga 5</label><br>
-            <input type="radio" name="qFive" value="one">1<br>
-            <input type="radio" name="qFive" value="two">2<br>
-            <input type="radio" name="qFive" value="three">3<br>
-            <input type="radio" name="qFive" value="four">4<br>
-            <input type="radio" name="qFive" value="five">5<br><br>
+            <label for="qFive">Fråga 5</label><br><br>
+            <input type="radio" name="qFive" id="q5a1" value="one">
+            <label for="q5a1">1</label>
+            <input type="radio" name="qFive" id="q5a2" value="two">
+            <label for="q5a2">2</label>
+            <input type="radio" name="qFive" id="q5a3" value="three">
+            <label for="q5a3">3</label>
+            <input type="radio" name="qFive" id="q5a4" value="four">
+            <label for="q5a4">4</label>
+            <input type="radio" name="qFive" id="q5a5" value="five">
+            <label for="q5a5">5</label><br><br><br>
             <input type="submit" value="Skicka" />
-
+        </form>
     </body>
 </html>
