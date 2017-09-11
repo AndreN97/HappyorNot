8eb9e85a548ae7a74bb94af15d269b1e854d8622
AndreN97 <97noan@skola.engelholm.se>
2017-09-11 07:34:58 +0200

    Början på sidans gränssnitt

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Document</title>
+</head>
+    <body>
+        <form action="result.php" method="post">
+            <label>Fråga 1</label><br>
+            <input type="radio" name="qOne" value="one">1<br>
+            <input type="radio" name="qOne" value="two">2<br>
+            <input type="radio" name="qOne" value="three">3<br>
+            <input type="radio" name="qOne" value="four">4<br>
+            <input type="radio" name="qOne" value="five">5<br><br>
+            <label>Fråga 2</label><br>
+            <input type="radio" name="qTwo" value="one">1<br>
+            <input type="radio" name="qTwo" value="two">2<br>
+            <input type="radio" name="qTwo" value="three">3<br>
+            <input type="radio" name="qTwo" value="four">4<br>
+            <input type="radio" name="qTwo" value="five">5<br><br>
+            <label>Fråga 3</label><br>
+            <input type="radio" name="qTwo" value="one">1<br>
+            <input type="radio" name="qTwo" value="two">2<br>
+            <input type="radio" name="qTwo" value="three">3<br>
+            <input type="radio" name="qTwo" value="four">4<br>
+            <input type="radio" name="qTwo" value="five">5<br><br>
+            <label>Fråga 4</label><br>
+            <input type="radio" name="qTwo" value="one">1<br>
+            <input type="radio" name="qTwo" value="two">2<br>
+            <input type="radio" name="qTwo" value="three">3<br>
+            <input type="radio" name="qTwo" value="four">4<br>
+            <input type="radio" name="qTwo" value="five">5<br><br>
+            <label>Fråga 5</label><br>
+            <input type="radio" name="qTwo" value="one">1<br>
+            <input type="radio" name="qTwo" value="two">2<br>
+            <input type="radio" name="qTwo" value="three">3<br>
+            <input type="radio" name="qTwo" value="four">4<br>
+            <input type="radio" name="qTwo" value="five">5<br><br>
+            <input type="submit" value="Skicka" />
+    </body>
+</html>
